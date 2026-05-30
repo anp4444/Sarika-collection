b2eec1b84aaa0f83d2d9b44baef76b207548d282
Ayush Parakh <aayushnileshparakh@gmail.com>
2026-05-31 00:30:50 +0530

    update

--- public/sw.js
+++ public/sw.js
@@ -1,23 +1,12 @@
-const CACHE = 'sarikacollection-v1';
-const ASSETS = ['/', '/about', '/contact', '/cart', '/manifest.json'];
-
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
+self.addEventListener('install', () => self.skipWaiting());
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
-    )
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(
-    caches.match(e.request).then((cached) => cached || fetch(e.request))
-  );
+  e.respondWith(fetch(e.request));
 });
